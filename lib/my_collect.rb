0f3c79447db88158635ca7d7c2bac0e2675acfad
Christopher K Swofford <christopher.swofford@gmail.com>
2019-03-23 23:05:53 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,5 @@
 
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end 
