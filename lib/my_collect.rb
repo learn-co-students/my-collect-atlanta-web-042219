f3893df7e502a3e4f5f48d39896fe30f69ee8582
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-31 23:16:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+    i = 0
+    new_array = []
+    while i < array.length
+      yield array[i]
+      item = array[i].split(" ").first
+      new_array.push(item)
+      i +=1
+    end
+    new_array
+end
 
 
+
+#
+# def my_array(array)
+#   array.each do |num|
+#     puts num
+#   end
+# end
