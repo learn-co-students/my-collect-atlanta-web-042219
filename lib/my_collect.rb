363150208f21c7f00c1c3262d1487c85e001b74d
github username <github email address>
2019-04-14 14:29:24 -0400

    controlled yield return value

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(collection)
+  new_collection = []
+  x = 0#num?
 
-
+  while x < collection.length #conditional statement
+    new_collection << yield(collection[x])
+    x += 1
+  end
+  new_collection
+  #return modified collection...does it need a new variable
+end
